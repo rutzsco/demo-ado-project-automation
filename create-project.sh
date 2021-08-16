023856568f5391d617ccdca41f730998b55a8035
Scott Rutz <scrutz@microsoft.com>
2021-08-16 12:08:11 -0500

    cp

--- create-project.sh
+++ create-project.sh
@@ -2,8 +2,13 @@
 # Bash script using Azure CLI to create am Azure DevOpsProject with ServiceConnection
 # This script reads command line parameters to define:
 # - $1 = Project Name
+# - $2 = Organization Uri
 #############################################################################
 
 adoProjectName=$1
+adoOrganizationUri=$2
 
 echo "adoProjectName: $adoProjectName"
+echo "adoOrganizationUri: $adoOrganizationUri"
+
+az devops project create --name $adoProjectName --organization $adoOrganizationUri
